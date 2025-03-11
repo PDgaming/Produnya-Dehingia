810b3abc9de5b409fed648ec11168f949ae36447
PD <89239979+PDgaming@users.noreply.github.com>
2025-03-12 03:22:38 +0530

    minor changes

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -3,19 +3,21 @@
 	import * as Extra from '@threlte/extras';
 	import { Spring } from 'svelte/motion';
 
-	Extra.interactivity();
 	const scale = new Spring(1);
 	let rotation = 0;
+
+	Extra.interactivity();
 	Threlte.useTask((delta) => {
 		rotation += delta;
 	});
 </script>
 
-<Threlte.T.DirectionalLight position={[0, 10, 10]} castShadow intensity={2} />
+<Threlte.T.DirectionalLight position={[0, 8.5, -7.5]} castShadow intensity={5} />
+<Threlte.T.DirectionalLight position={[7.5, 8.5, 0]} castShadow intensity={2} />
 
 <Threlte.T.PerspectiveCamera
 	makeDefault
-	position={[10, 10, 15]}
+	position={[9, 11, -11]}
 	oncreate={(ref) => {
 		ref.lookAt(0, 4, 0);
 	}}
@@ -27,11 +29,10 @@
 	url="modals/man_working_on_laptop.glb"
 	position.y={1.1}
 	args={[0.5, 0.5, 0.5]}
-	rotation={[0, rotation, 0]}
 	castShadow
 />
 
-<Threlte.T.Mesh rotation.x={-Math.PI / 2} receiveShadow position.y={1} rotation.z={rotation}>
-	<Threlte.T.BoxGeometry args={[15, 13, 0.1]} />
+<Threlte.T.Mesh rotation.x={-Math.PI / 2} receiveShadow position.y={1}>
+	<Threlte.T.BoxGeometry args={[15, 15, 0.1]} />
 	<Threlte.T.MeshStandardMaterial color="white" />
 </Threlte.T.Mesh>
