f5ff5ae74b5bbfe8ae9edd96b0052513f3c23bce
PDgaming <produnyadehingia12345@gmail.com>
2026-07-03 09:15:26 +0530

    removed

--- src/routes/components/aboutMe.svelte
+++ src/routes/components/aboutMe.svelte
@@ -4,30 +4,27 @@
 </script>
 
 <div
-	class="about-me absolute flex flex-col items-center justify-center gap-4 bg-base-100"
+	class="about-me bg-base-100 absolute flex flex-col items-center justify-center gap-4"
 	use:draggable={{ bounds: 'body', handle: '.title-bar' }}
 >
-	<div class="title-bar absolute left-0 top-0 w-full">
+	<div class="title-bar absolute top-0 left-0 w-full">
 		<TitleBar />
 	</div>
 	<div class="avatar avatar-placeholder">
 		<div
-			class="w-24 rounded-full bg-orange-400 text-neutral-content ring ring-primary ring-offset-2 ring-offset-base-100"
+			class="text-neutral-content ring-primary ring-offset-base-100 w-24 rounded-full bg-orange-400 ring ring-offset-2"
 		>
 			<span class="text-3xl">PD</span>
 		</div>
 	</div>
 	<div class="content flex flex-col gap-2">
 		<div class="title flex flex-col items-center">
-			<h2 class="text-3xl">Produnya Dehingia</h2>
+			<h2 class="text-3xl">ProDeh</h2>
 			<h3 class="text-1xl">Full Stack Developer</h3>
 		</div>
-		<div class="card w-96 bg-base-300 shadow-sm">
+		<div class="card bg-base-300 w-96 shadow-sm">
 			<div class="card-body">
-				<p>
-					I am Produnya Dehingia, a 15-year-old coder from Assam with over 6 years of experience in
-					programming.
-				</p>
+				<p>I am ProDeh, a 16-year-old coder with over 7 years of experience in programming.</p>
 			</div>
 		</div>
 	</div>
